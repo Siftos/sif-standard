b60db412e183d7b650b637f386fcbca569b1ae31
Siftos <47024913+Siftos@users.noreply.github.com>
2023-10-12 15:33:39 +0300

    Update ui.lua

--- ui.lua
+++ ui.lua
@@ -15,48 +15,7 @@ navbar = {
     items = {}
 }
 
-window = {
-    x = 3,
-    y = 3,
-    width = 15,
-    height = 30,
-    name = "Window",
-    enabled = false,
-    fullscreen = false,
-    background_color = colors.white,
-    topbar_color = colors.gray,
-    topbar_textcolor = colors.white,
-    highlighted_exit = true,
-    items = {}
-}
 --functions
-function window:item()
-
-end
-
-function window:paint()
-    for dx = 0, self.width - 1 do
-        for dy = 0, self.height - 1 do
-            term.setCursorPos(self.x + dx, self.y + dy)
-            term.setBackgroundColor(self.background_color)
-            if dy == 0 then
-                term.setBackgroundColor(self.topbar_color)
-            end
-            term.write(" ")
-        end
-    end
-    term.setCursorPos(self.x, self.y)
-    term.setBackgroundColor(self.topbar_color)
-    term.setTextColor(self.topbar_textcolor)
-    term.write(self.name)
-    term.setCursorPos(self.x + self.width - 1, self.y)
-    term.setTextColor(colors.red)
-    if self.highlighted_exit then
-        term.setBackgroundColor(colors.red)
-        term.setTextColor(colors.white)
-    end
-    write("x")
-end
 
 function navbar:item(_text, _align, _highlighted)
     table.insert(self.items, #self.items + 1, {_text, _align, _highlighted})
